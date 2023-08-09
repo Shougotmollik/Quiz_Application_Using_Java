f0cef0d5f3c2fa45a374d2ead5e06bdd8ecdb0fc
Shougotmollik <shougotmollik@gmail.com>
2023-08-09 22:21:16 +0600

    First Commit

---
+++ src/Login.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Login extends JFrame {
+    Login(){
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Login();
+    }
+}
